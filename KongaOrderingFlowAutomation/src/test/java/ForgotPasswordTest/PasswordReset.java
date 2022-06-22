92e473dd732263bf1fcd69da576a58ca57993950
OloriT <oloritaiwo@gmail.com>
2022-06-22 21:45:29 +0100

    Added Konga Ordering Flow Automation Project

---
+++ KongaOrderingFlowAutomation/src/test/java/ForgotPasswordTest/PasswordReset.java
@@ -0,0 +1,53 @@
+package ForgotPasswordTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class PasswordReset {
+
+    //import the selenium web driver
+    private WebDriver driver;
+
+    @BeforeTest
+    public void start() throws InterruptedException {
+        //locate where the chromedriver is
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        //1. open your Chrome browser
+        driver = new ChromeDriver();
+        //2. input your Konga Browser URL (https://www.konga.com/)
+        driver.get("https://www.konga.com/");
+        Thread.sleep(3000);
+        //3. Maximise the browser
+        driver.manage().window().maximize();
+        //4. click on the Login/signup button to open the Login/Signup page
+        driver.findElement(By.xpath("//*[@id=\"nav-bar-fix\"]/div[1]/div/div/div[4]/a")).click();
+        Thread.sleep(3000);
+
+    }
+
+    @Test(priority = 0)
+    public void ForgotPassword () throws InterruptedException {
+        //5. click on forgot password to reset password
+        driver.findElement(By.xpath("//*[@id=\"app-content-wrapper\"]/div[4]/section/section/aside/div[2]/div/form/div[2]/label/a")).click();
+        Thread.sleep(3000);
+    }
+    @Test(priority = 1)
+    public void ValidEmail () throws InterruptedException {
+        //Enter email in the email field
+        driver.findElement(By.id("email")).sendKeys("oloritaiwo@gmail.com");
+        Thread.sleep(3000);
+        //Click on set reset Link
+        driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div/div/div/div[1]/div/form/div[2]/button")).click();
+
+    }
+
+    @AfterTest
+    public void closeBrowser () {
+        //Quit the browser
+        driver.quit();
+    }
+}
---
+++ KongaOrderingFlowAutomation/src/test/java/ValidLoginTest/KongaUserLogin.java
@@ -0,0 +1,79 @@
+package ValidLoginTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class KongaUserLogin {
+
+    //import the selenium WebDriver
+    private WebDriver driver;
+
+    @BeforeTest
+    public void start() throws InterruptedException {
+        //locate where the chromedriver is
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        //1. open your Chrome browser
+        driver = new ChromeDriver();
+        //2. input your Konga Browser URL (https://www.konga.com/)
+        driver.get("https://www.konga.com/");
+        Thread.sleep(3000);
+        //3. Maximise the browser
+        driver.manage().window().maximize();
+        //4. click on the Login/signup button to open the Login/Signup page
+        driver.findElement(By.xpath("//*[@id=\"nav-bar-fix\"]/div[1]/div/div/div[4]/a")).click();
+        Thread.sleep(3000);
+
+    }
+
+    @Test(priority = 0)
+    public void ValidSignup () throws InterruptedException {
+        //5. Input your email address or phone number in email/phone number field
+        driver.findElement(By.id("username")).sendKeys("oloritaiwo@gmail.com");
+        //6. Input your password in the password field
+        driver.findElement(By.id("password")).sendKeys("WisdomOlufemi5488");
+        //7. Click on log in to log in to your account
+        driver.findElement(By.xpath("//*[@id=\"app-content-wrapper\"]/div[4]/section/section/aside/div[2]/div/form/div[3]/button")).click();
+        Thread.sleep(3000);
+    }
+
+    @Test(priority = 1)
+    public void SearchItem () throws InterruptedException {
+        //8. Click on phones and tablet to see list
+        driver.findElement(By.xpath("//*[@id=\"nav-bar-fix\"]/div[2]/div/a[3]")).click();
+        Thread.sleep(3000);
+        //9. Add first item to my cart
+        driver.findElement(By.xpath("//*[@id=\"mainContent\"]/section[3]/section/section/section[2]/section/ul/li[1]/div/div/div[2]/form/div[3]/button")).click();
+        Thread.sleep(3000);
+        //10. Add second item to my cart
+        driver.findElement(By.xpath("//*[@id=\"mainContent\"]/section[3]/section/section/section[2]/section/ul/li[4]/div/div/div[2]/form/div[3]/button")).click();
+        Thread.sleep(3000);
+    }
+    @Test(priority = 2)
+            public void CheckCart() throws InterruptedException {
+        //11. Go to my cart
+        driver.findElement(By.xpath("//*[@id=\"nav-bar-fix\"]/div[1]/div/div/a[2]/span[1]")).click();
+        Thread.sleep(3000);
+        //12. Close Cart Overview
+        driver.findElement(By.xpath("//*[@id=\"app-content-wrapper\"]/div[3]/section/section/aside/div[1]/div/a/span")).click();
+        Thread.sleep(3000);
+        //13. Click on my account
+        driver.findElement(By.xpath("//*[@id=\"nav-bar-fix\"]/div[1]/div/div/div[4]/div/a/span")).click();
+        Thread.sleep(3000);
+        //Log out
+        driver.findElement(By.xpath("//*[@id=\"nav-bar-fix\"]/div[1]/div/div/div[4]")).click();
+        Thread.sleep(3000);
+    }
+
+        @AfterTest
+        public void closeBrowser () {
+            //Quit the browser
+            driver.quit();
+
+
+    }
+
+}
